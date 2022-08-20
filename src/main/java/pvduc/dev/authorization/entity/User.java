36fe998e5a5df45205a8858b3e42b7d93493ee38
pvduc <pvduc.dev@gmail.com>
2022-08-20 16:56:26 +0700

    chore: Initial project config

--- src/main/java/pvduc/dev/authorization/entity/User.java
+++ src/main/java/pvduc/dev/authorization/entity/User.java
@@ -1,2 +1,45 @@
-package pvduc.dev.authorization.entity;public class User {
+package pvduc.dev.authorization.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class User {
+	@Id
+	@Type(type="uuid-char")
+	private UUID id;
+
+	@NotNull
+	private String email;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String avatar;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+		User user = (User) o;
+		return id != null && Objects.equals(id, user.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
 }
--- src/main/java/pvduc/dev/authorization/repository/UserRepository.java
+++ src/main/java/pvduc/dev/authorization/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package pvduc.dev.authorization.repository;public interface UserRepository {
+package pvduc.dev.authorization.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pvduc.dev.authorization.entity.User;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
